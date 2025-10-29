1058c7b42f248e1419ee6ccc5e86eb1847c70a5d
Primefox59 <primetroyxs@gmail.com>
2025-10-29 19:10:29 +0700

    1

--- app.py
+++ app.py
@@ -1640,7 +1640,7 @@ def page_auth():
     col1, col2, col3, col4, col5 = st.columns([1, 1, 2, 1, 1])
     
     with col3:
-        st.image("logo.png", width=500)
+        st.image("logo.png", width=550)
         st.markdown("---")
             
         tab = st.tabs(["Login", "Register"])
